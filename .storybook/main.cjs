c8b4adad45e0aed1604944fcd815e9fc550a6bb5
Viktor Avelino <viktor.avelino@gmail.com>
2022-10-12 19:26:29 -0400

    ci: Add / to config.base main file storybook

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -14,7 +14,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/ignite-lab-ds';
+      config.base = '/ignite-lab-ds/';
     }
 
     return config;
